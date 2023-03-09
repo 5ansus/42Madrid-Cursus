946088bf2f161be79e151750c817de5f4cd50576
Santiago Fernandez Herrera <sanferna@c2r11s2.42madrid.com>
2023-03-09 23:25:04 +0100

    Header norminetted

--- libft/libft.h
+++ libft/libft.h
@@ -1,6 +1,20 @@
-#ifndef LIBFT
-#define LIBFT
-#include <stddef.h>
-int	ft_strlen(const char *s);
-void *ft_memset(void *b, int c, size_t len);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanferna <sanferna@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/09 23:17:38 by sanferna          #+#    #+#             */
+/*   Updated: 2023/03/09 23:24:47 by sanferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+int		ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
+
 #endif
