059d2470af69bf08092b8b165f17db5b8a615af7
5ansus <99260279+5ansus@users.noreply.github.com>
2023-03-21 16:56:03 +0100

    Update ft_strnstr.c

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,26 +6,21 @@
 /*   By: sanferna <sanferna@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:45:11 by sanferna          #+#    #+#             */
-/*   Updated: 2023/03/21 15:56:08 by sanferna         ###   ########.fr       */
+/*   Updated: 2023/03/21 16:55:21 by sanferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	len_haystack;
+	char	*ret;
 	size_t	len_needle;
-	size_t	i;	
-	size_t	j;
 
-	i = 0;
-	len_haystack = ft_strlen(haystack);
 	len_needle = ft_strlen(needle);
-	while (haystack[i] != '\0' && i < len)
+	while (ret = ft_strchr(haystack, needle[0]))
 	{
-		if (haystack[i] == needle[0]){
-		
-		}
+		if ((ret + len_needle - haystack < len) && ft_strncmp(needle, ret, len_needle) == 0)
+			return (ret);
 	}
 	return (NULL);
 }
