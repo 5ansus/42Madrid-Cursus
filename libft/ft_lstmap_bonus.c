02ec29517014d7fb45299fdd3e5185400d8904a0
5ansus <sanferna@student.42madrid.com>
2023-04-06 20:25:55 +0200

    Libft terminada

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sanferna <sanferna@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 18:09:20 by sanferna          #+#    #+#             */
-/*   Updated: 2023/04/06 18:57:01 by sanferna         ###   ########.fr       */
+/*   Updated: 2023/04/06 20:24:29 by sanferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	while (node != NULL)
 	{
 		content = f(node->content);
-		if (content == NULL)
-		{
-			if(ret == NULL)
-				return (NULL);
-			else
-				return (ft_lstclear(&ret, del), NULL);
-		}
 		new_node = ft_lstnew(content);
 		if (new_node == NULL)
 		{
-			if(ret == NULL)
-				return (del(content), NULL);
-			else
-				return (ft_lstclear(&ret, del), NULL);
+			del(content);
+			return (ft_lstclear(&ret, del), NULL);
 		}
 		ft_lstadd_back(&ret, new_node);
 		node = node->next;
