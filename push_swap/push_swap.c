84f88d8b34d37bf46d28a99d98104d30dc89d94e
5ansus <sanferna@student.42madrid.com>
2024-08-24 18:41:08 +0200

    Push swap
    nt working

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: sanferna <sanferna@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 17:08:04 by sanferna          #+#    #+#             */
-/*   Updated: 2024/08/24 12:18:20 by sanferna         ###   ########.fr       */
+/*   Updated: 2024/08/24 18:39:16 by sanferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ static void	order_stack(t_llist *stacks[], int first, int size, int pos);
 static void	divide_stack(t_llist *stacks[], int first, int size, int pos);
 static void	divide_from_A(t_llist *stacks[], int first, int size, int pos);
 static void	divide_from_B(t_llist *stacks[], int first, int size, int pos);
+// static void	stack_of_five(t_llist *stacks[], int first, int size, int pos);
 int	main(int argc, char **argv)
 {
 	t_llist	*stacks[2];
@@ -33,6 +34,8 @@ int	main(int argc, char **argv)
 	{
 		ft_llst_print(&stacks[A], printer);
 		order_stack(stacks, 1, n_numbers, TOP_A);
+		ft_llst_print(&stacks[A], printer);
+		ft_llst_print(&stacks[B], printer);
 		ft_llst_clear(&stacks[A], free);
 	}
 	return (0);
@@ -44,17 +47,18 @@ static void	order_stack(t_llist *stacks[], int first, int size, int pos)
 
 	div = size / 3;
 	if (size <= 5)
-	{
-		if (pos == TOP_A)
-			ft_printf("Ubicación: TOP_A\n");
-		if (pos == BOT_A)
-			ft_printf("Ubicación: BOT_A\n");
-		if (pos == TOP_B)
-			ft_printf("Ubicación: TOP_B\n");
-		if (pos == BOT_B)
-			ft_printf("Ubicación: BOT_B\n");
-		ft_printf("Fin de la recursión. Rango: %d -- %d\n", first, first + size - 1);
-	}
+		// stack_of_five(stacks, first, size, pos);
+		{
+				if (pos == TOP_A)
+		ft_printf("Ubicación: TOP_A\n");
+	if (pos == BOT_A)
+		ft_printf("Ubicación: BOT_A\n");
+	if (pos == TOP_B)
+		ft_printf("Ubicación: TOP_B\n");
+	if (pos == BOT_B)
+		ft_printf("Ubicación: BOT_B\n");
+	ft_printf("Fin de la recursión. Rango: %d -- %d\n", first, first + size - 1);
+		}
 	else
 	{
 		ft_printf("Empezando, hay que dividir %d elementos, empezando por %d\n", size, first);
@@ -97,16 +101,17 @@ static void	divide_from_A(t_llist *stacks[], int first, int size, int pos)
 	{
 		content = (int *) (*stacks)[A].content;
 		if (pos == BOT_A)
-			reverse_rotate(&(stacks[A]), STACK_A);
+			reverse_rotate(stacks, STACK_A);
 		if (*content < 2 * div + first)
 		{
 			push(stacks, STACK_B);
 			if (*content< div + first)
-				rotate(&(stacks[B]), STACK_B);
+				rotate(stacks, STACK_B);
 		}
 		else if (pos == TOP_A)
-			rotate(&(stacks[A]), STACK_A);
+			rotate(stacks, STACK_A);
 		i++;
+		ft_printf(">>%d\n", i);
 	}
 }
 
@@ -122,15 +127,30 @@ static void	divide_from_B(t_llist *stacks[], int first, int size, int pos)
 	{
 		content = (int *) (*stacks)[B].content;
 		if (pos == BOT_B)
-			reverse_rotate(&(stacks[B]), STACK_B);
+			reverse_rotate(stacks, STACK_B);
 		if (*content < div + first && pos == TOP_B)
-			rotate(&(stacks[B]), STACK_B);
+			rotate(stacks, STACK_B);
 		else if (*content < 2 * div + first)
 		{
 			push(stacks, STACK_A);
 			if (*content < div + first)
-				rotate(&(stacks[A]), STACK_B);
+				rotate(stacks, STACK_B);
 		}
 		i++;
 	}
 }
+
+// static void	stack_of_five(t_llist *stacks[], int first, int size, int pos)
+// {
+// 	if (pos == TOP_A)
+// 		ft_printf("Ubicación: TOP_A\n");
+// 	if (pos == BOT_A)
+// 		ft_printf("Ubicación: BOT_A\n");
+// 	if (pos == TOP_B)
+// 		ft_printf("Ubicación: TOP_B\n");
+// 	if (pos == BOT_B)
+// 		ft_printf("Ubicación: BOT_B\n");
+// 	ft_printf("Fin de la recursión. Rango: %d -- %d\n", first, first + size - 1);
+// 	ft_llst_print(&stacks[A], printer);
+// 	ft_printf("-------------\n");
+// }
