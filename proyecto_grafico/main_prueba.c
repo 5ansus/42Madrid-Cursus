efa9dcc5acf4608c2f84b88f989bc52941c5b215
5ansus <sanferna@student.42madrid.com>
2025-02-22 17:41:17 +0100

    Minilibx test
    First aprroach to MinilibX

--- proyecto_grafico/main_prueba.c
+++ proyecto_grafico/main_prueba.c
@@ -1,25 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_prueba.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanferna <sanferna@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 17:10:32 by sanferna          #+#    #+#             */
+/*   Updated: 2025/02/22 17:31:00 by sanferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minilibx-linux/mlx.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 int finish_win(void *mlx)
 {
-    mlx_loop_end(mlx);
-    return (26);
+	mlx_loop_end(mlx);
+	return (26);
 }
+
+typedef struct s_image {
+
+	void	*p;
+	int		height;
+	int		witdth;
+} t_image;
 
 int main(void)
 {
-    void *mlx;
-    void *win;
+	void *mlx;
+	void *win;
+	t_image img;
 
-    mlx = mlx_init();
-    if (mlx == NULL)
-        return (0);
-    win = mlx_new_window(mlx, 800, 600, "Prueba");
-    mlx_hook(win, 17, 1<<17, finish_win, mlx);
-    mlx_loop(mlx);
-    mlx_destroy_window(mlx, win);
-    mlx_destroy_display(mlx);
-    free(mlx);
+	mlx = mlx_init();
+	if (mlx == NULL)
+		return (0);
+	win = mlx_new_window(mlx, 800, 600, "Prueba");
+	img.p = mlx_xpm_file_to_image(mlx, "sprites/coin.xpm", &img.witdth, &img.height);
+	mlx_put_image_to_window(mlx, win, img.p, 0, 0);
+	mlx_hook(win, 17, 1<<17, finish_win, mlx);
+	mlx_loop(mlx);
+	mlx_destroy_window(mlx, win);
+	mlx_destroy_display(mlx);
+	free(mlx);
 }
